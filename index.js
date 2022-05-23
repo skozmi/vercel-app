48cb4180ee0ebb729716defbcffd0d16ef5f550d
skozmi <skozmi@gmail.com>
2022-05-23 21:42:01 +0200

    version 1.0

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//app.use(express.static("public"));
+
+
+app.get('/', (req, res, next)=>{
+    res.send('Express on Vercel');
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
+//Export the Express API
+module.exports = app;

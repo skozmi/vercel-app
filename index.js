9eca97af2a3e8edd173538627ebda3eb421047e1
skozmi <skozmi@gmail.com>
2022-05-24 12:36:57 +0200

    prepare for ejs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ejs = require('ejs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,10 +8,12 @@ const PORT = process.env.PORT || 3000;
 //app.use("/client", express.static(__dirname + "/client"));
 app.use("/public", express.static(__dirname + "/public"));
 
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next)=>{
     //res.send('Express on Vercel');
-    res.sendFile(__dirname + '/index.html');
+    //res.sendFile(__dirname + '/index.html');
+    res.render('main');
 });
 
 app.listen(PORT, ()=> {

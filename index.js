a307d208100b1f2198d12e96a16bcf2a34b19a3a
skozmi <skozmi@gmail.com>
2022-05-24 10:49:20 +0200

    updates files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "/css"));
 
 
 app.get('/', (req, res, next)=>{
